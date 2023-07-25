a81e81fcabaa3b93c5880acdb5283b509ebd003e
hyunjae000467 <hyunjae000467@gmail.com>
2023-07-25 21:32:34 +0900

    RSI now Shows better results like Stochastic.

--- Modules/RSI_Analyzer/RSI_Analyzer.py
+++ Modules/RSI_Analyzer/RSI_Analyzer.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def rsi_calc(coin_name):
@@ -20,17 +20,47 @@ def rsi_calc(coin_name):
     df['AD'] = df['하락폭'].ewm(span=14, min_periods=14, adjust=False).mean()
     df['RSI'] = df['AU'] / (df['AU'] + df['AD']) * 100
 
-    return df[['RSI']]
+    # Generate Buy and Sell Signals
+    buy_signals = df['RSI'] < 30
+    sell_signals = df['RSI'] > 70
+
+    current_signal = "Neutral"
+    if buy_signals.iloc[-1]:
+        current_signal = "Buy"
+    elif sell_signals.iloc[-1]:
+        current_signal = "Sell"
+
+    return df, df['RSI'], buy_signals, sell_signals, current_signal
 
 
-def rsi_grph(coin_name):
-    df = rsi_calc(coin_name)
+def rsi_grph(coin_name, recent_hours=6):
+    df, rsi, buy_signals, sell_signals, current_signal = rsi_calc(coin_name)
+
+    # Get the end time of the data
+    end_time = df.index[-1]
+
+    # Calculate the start time for the last 6 hours
+    start_time = end_time - timedelta(hours=recent_hours)
+
+    # Slice the data for the last 6 hours
+    recent_df = df[df.index >= start_time]
+    recent_rsi = rsi[rsi.index >= start_time]
+    recent_buy_signals = buy_signals[buy_signals.index >= start_time]
+    recent_sell_signals = sell_signals[sell_signals.index >= start_time]
+
     plt.close('all')
     plt.figure(figsize=(12, 6))
-    plt.plot(df.index, df['RSI'], label='RSI', color='b')
+    plt.plot(recent_df.index, recent_rsi, label='RSI', color='b')
     plt.axhline(y=70, color='r', linestyle='--', label='Overbought (70)')
     plt.axhline(y=30, color='g', linestyle='--', label='Oversold (30)')
-    plt.title('RSI Graph for {}'.format(coin_name))
+
+    # Plot buy and sell signals
+    plt.plot(recent_df.index[recent_buy_signals], recent_rsi[recent_buy_signals], '^', markersize=10, color='g',
+             label='Buy Signal')
+    plt.plot(recent_df.index[recent_sell_signals], recent_rsi[recent_sell_signals], 'v', markersize=10, color='r',
+             label='Sell Signal')
+
+    plt.title('RSI Graph for {} - Last 6 Hours'.format(coin_name))
     plt.xlabel('Date')
     plt.ylabel('RSI Value')
     plt.legend()
@@ -41,6 +71,21 @@ def rsi_grph(coin_name):
     plt.text(0.01, 0.01, 'Current Time: {}'.format(current_time), transform=plt.gca().transAxes,
              fontsize=10, verticalalignment='bottom', bbox=dict(facecolor='white', alpha=0.5))
 
+    # Add a text box on the top right
+    box_text = ""
+    text_color = "gray"
+    if current_signal == "Buy":
+        box_text = "BUY"
+        text_color = "red"
+    elif current_signal == "Sell":
+        box_text = "SELL"
+        text_color = "green"
+    elif current_signal == "Neutral":
+        box_text = "Neutral"
+
+    plt.text(0.98, 0.98, box_text, transform=plt.gca().transAxes, fontsize=12, color=text_color,
+             verticalalignment='top', horizontalalignment='right', bbox=dict(facecolor='white', edgecolor='black'))
+
     # Create the 'RSI_IMG_FILES' directory if it does not exist
     img_dir = 'Modules/RSI_Analyzer/RSI_IMG_FILES'
     if not os.path.exists(img_dir):
@@ -49,3 +94,5 @@ def rsi_grph(coin_name):
     resource_location = os.path.join(img_dir, 'RSI_{}.png'.format(coin_name))
     plt.savefig(resource_location)
     # print("RSI Done")
+
+    return recent_df, recent_rsi, recent_buy_signals, recent_sell_signals, current_signal
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pyupbit
-import Modules.RSI_Analyzer.RSI_Analyzer as Rsi
 import Modules.MACD_Analyzer.MACD_Analyzer as Macd
+import Modules.RSI_Analyzer.RSI_Analyzer as Rsi
 import Modules.Stochastic_Analyzer.Stochastic_Analyzer as Stct
 
 input_file = './Modules/Get_Coins_Data/clist'
@@ -26,16 +25,20 @@ progress = 0
 for coin in coin_list:
     progress += 1
     if mode == 1:
+        rsi_df, rsi_df_2, rsi_buy_signals, rsi_sell_signals, rsi_current_signal = Rsi.rsi_calc(coin)
         Rsi.rsi_grph(coin)
-        print("[", round(progress / 30 * 100), "%] RSI Done on coin ", coin)
+        print("[", round(progress / 30 * 100), "%] RSI Done on coin ", coin, ", Coin is currently on",
+              rsi_current_signal, "position.")
     elif mode == 2:
         Macd.macd_grph(coin)
         print("[", round(progress / 30 * 100), "%] MACD Done on coin ", coin)
     elif mode == 3:
-        stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals, stct_current_signal = Stct.stochastic_calc(coin,
-                                                                                                             'minute5',
-                                                                                                             1440)
-        Stct.stochastic_grph(coin, stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals, stct_current_signal)
+        stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals, stct_current_signal = Stct.stochastic_calc(
+            coin,
+            'minute5',
+            1440)
+        Stct.stochastic_grph(coin, stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals,
+                             stct_current_signal)
         print("[", round(progress / 30 * 100), "%] STCT Done on coin ", coin, ", Coin is currently on",
               stct_current_signal, "position.")
     else:
