d6dd9503c4eeb839630b3608befc00cb86a23e93
OrangeZone <hyunjae000467@gmail.com>
2023-08-25 00:47:36 +0900

    Add ADX algorithm

---
+++ Modules/ADX_Analyzer/ADX_Analyzer.py
@@ -0,0 +1,68 @@
+from datetime import datetime, timedelta
+import numpy as np
+import matplotlib.pyplot as plt
+import pyupbit
+import os
+
+
+def calculate_adx(ticker, interval, period, threshold=25):
+    df = pyupbit.get_ohlcv(ticker, interval=interval)
+    df['true_range'] = np.maximum(df['high'] - df['low'], np.maximum(abs(df['high'] - df['close'].shift(1)),
+                                                                     abs(df['low'] - df['close'].shift(1))))
+    df['up_move'] = df['high'] - df['high'].shift(1)
+    df['down_move'] = df['low'].shift(1) - df['low']
+    df['plus_dm'] = np.where((df['up_move'] > df['down_move']) & (df['up_move'] > 0), df['up_move'], 0)
+    df['minus_dm'] = np.where((df['down_move'] > df['up_move']) & (df['down_move'] > 0), df['down_move'], 0)
+    df['tr_sum'] = df['true_range'].rolling(window=period).sum()
+    df['plus_di'] = (df['plus_dm'].rolling(window=period).sum() / df['tr_sum']) * 100
+    df['minus_di'] = (df['minus_dm'].rolling(window=period).sum() / df['tr_sum']) * 100
+    df['adx'] = (abs(df['plus_di'] - df['minus_di']) / (df['plus_di'] + df['minus_di'])) * 100
+
+    current_plus_di = df['plus_di'].iloc[-1]
+    current_minus_di = df['minus_di'].iloc[-1]
+    current_signal = "Neutral"
+    if current_plus_di > current_minus_di:
+        current_signal = "Buy" if current_plus_di > threshold else "Neutral"
+    elif current_plus_di < current_minus_di:
+        current_signal = "Sell" if current_minus_di > threshold else "Neutral"
+
+    return df, df['adx'], current_signal
+
+
+def adx_grph(coin_name, df, adx, plus_di, minus_di, current_signal, threshold=25):
+    end_time = df.index[-1]
+    start_time = end_time - timedelta(hours=6)
+    recent_df = df[df.index >= start_time]
+    recent_adx = adx[adx.index >= start_time]
+    recent_plus_di = plus_di[plus_di.index >= start_time]
+    recent_minus_di = minus_di[minus_di.index >= start_time]
+
+    plt.close('all')
+    plt.figure(figsize=(12, 6))
+    plt.plot(recent_df.index, recent_adx, label='ADX', color='orange')
+    plt.plot(recent_df.index, recent_plus_di, label='+DI', color='blue')
+    plt.plot(recent_df.index, recent_minus_di, label='-DI', color='red')
+    plt.axhline(threshold, color='gray', linestyle='dashed', alpha=0.7, label='Threshold')
+
+    buy_signals = recent_df['plus_di'] > recent_df['minus_di']
+    sell_signals = recent_df['plus_di'] < recent_df['minus_di']
+
+    plt.fill_between(recent_df.index, 0, 100, where=buy_signals, facecolor='green', alpha=0.3, label='Buy Area')
+    plt.fill_between(recent_df.index, 0, 100, where=sell_signals, facecolor='red', alpha=0.3, label='Sell Area')
+
+    plt.title('ADX and DI for {} - Last 6 Hours'.format(coin_name))
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.legend()
+    plt.grid()
+
+    current_time = datetime.now().strftime('%Y/%m/%d-%H:%M:%S')
+    plt.text(0.01, 0.01, 'Current Time: {}'.format(current_time), transform=plt.gca().transAxes,
+             fontsize=10, verticalalignment='bottom', bbox=dict(facecolor='white', alpha=0.5))
+
+    img_dir = 'Modules/ADX_Analyzer/ADX_IMG_FILES'
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir)
+
+    resource_location = os.path.join(img_dir, 'ADX_{}.png'.format(coin_name))
+    plt.savefig(resource_location)
--- NonTradingCheck.py
+++ NonTradingCheck.py
@@ -4,6 +4,8 @@ import Modules.MACD_Analyzer.MACD_Analyzer_old as Macd
 import Modules.RSI_Analyzer.RSI_Analyzer_old as Rsi
 import Modules.Stochastic_Analyzer.Stochastic_Analyzer_old as Stct
 import Modules.Tensorflow_Analyzer.Tensorflow_Analyzer as Tf
+import Modules.ADX_Analyzer.ADX_Analyzer as Adx
+
 input_file = './Modules/Get_Coins_Data/clist'
 coin_list = []
 
@@ -18,51 +20,62 @@ print("Input 1 for RSI Analyze")
 print("Input 2 for MACD Analyze")
 print("Input 3 for Stochastic Oscillator Analyze")
 print("Input 4 for Tensorflow Prediction")
-print("Input 5 for ALL")
+print("Input 5 for ADX Analyze")
+print("Input 6 for ALL")
 mode = int(input("Select Mode : "))  # Change Mode for RSI, MACD, STOCHASTIC OSCILLATOR
 
 # 1 is for RSI
 # 2 is for MACD
 # 3 is for STOCHASTIC OSCILLATOR
 # 4 is for Tensorflow
-# 5 is for ALL
+# 5 is for ADX
+# 6 is for ALL
 progress = 0
 
 for coin in coin_list:
     if mode != 5:
         progress += 1
-    if mode == 1 or mode == 5:
+    if mode == 1 or mode == 6:
         rsi_df, rsi_df_2, rsi_buy_signals, rsi_sell_signals, rsi_current_signal = Rsi.rsi_calc(coin)
         Rsi.rsi_grph(coin)
-        if mode == 5:
+        if mode == 6:
             progress += (1 / 4)
         print("[", round(progress / 30 * 100), "%] RSI Done on coin ", coin, ", Coin is currently on",
               rsi_current_signal, "position.")
-    if mode == 2 or mode == 5:
+    if mode == 2 or mode == 6:
         macd_df, macd_line, macd_signal_line, macd_buy_signal, macd_sell_signal, macd_current_signal = Macd.macd_calc(
             coin)
         Macd.macd_grph(coin, macd_df, macd_line, macd_signal_line, macd_buy_signal, macd_sell_signal,
                        macd_current_signal)
-        if mode == 5:
+        if mode == 6:
             progress += (1 / 4)
         print("[", round(progress / 30 * 100), "%] MACD Done on coin ", coin, ", Coin is currently on",
               macd_current_signal, "position.")
-    if mode == 3 or mode == 5:
+    if mode == 3 or mode == 6:
         stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals, stct_current_signal = Stct.stochastic_calc(
             coin,
             'minute5',
             1440)
         Stct.stochastic_grph(coin, stct_df, k_percent, d_percent, stct_buy_signals, stct_sell_signals,
                              stct_current_signal)
-        if mode == 5:
+        if mode == 6:
             progress += (1 / 4)
         print("[", round(progress / 30 * 100), "%] STCT Done on coin ", coin, ", Coin is currently on",
               stct_current_signal, "position.")
-    if mode == 4 or mode == 5:
+    if mode == 4 or mode == 6:
         Tf.asset_price_prediction(coin_name=coin, interval='minute5', count=1440)
-        if mode == 5:
+        if mode == 6:
             progress += (1 / 4)
         print("[", round(progress / 30 * 100), "%] TF Done on coin ", coin)
+    if mode == 5 or mode == 6:
+        df, adx, adx_current_signal= Adx.calculate_adx(coin, 'minute5', 14)
+        plus_di = df['plus_di']
+        minus_di = df['minus_di']
+        Adx.adx_grph(coin, df, adx, plus_di, minus_di, adx_current_signal, threshold=25)
+
+        if mode == 6:
+            progress += (1 / 4)
+        print("[", round(progress / 30 * 100), "%] ADX Done on coin ", coin)
     if mode == 0 or math.isnan(mode) == True:
         print("[STOP] Program Ended | Cause : Mode is on 0, or NaN")
         break
